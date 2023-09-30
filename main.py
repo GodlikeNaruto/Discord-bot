b501fb9ce99da5d80654c92dd71c1eaaf6bd6892
Abduvohid <143887853+GodlikeNaruto@users.noreply.github.com>
2023-09-29 20:20:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,22 @@ import random
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='$', intents=intents)
+anecdots = ['Изобрели немцы часы которые услышав мат переходили на одну минуту вперед. Решили провести эксперимент: поставят они значит часы в американский бар, \nфранцузкий бар и русский заодно. И вот поставили часы в французкий бар и ушли, через день вернулись \nглядят часы на 3 минуты опережают. Поставили в американский бар через день вернулись \nопережают на 8 минут. Поставили в русский бар через день приходят часов \nнет спрашивают бармена где часы. А он "какие часы?". "Да вон там \nна стене висели" говорят немцы. "А это часы были? А я думал на##й \nнам вентилятор зимой нужен" .(Было так много мата что стрелки часов постоянно \nдвигались как вентилятор)', 
+'Была одна проблемная лошадь. отвез хозяин лошади ее к ветеринару говорит мол не двигается и не ест. Ветеринар дает таблетки и говорит что елси через 5 дней неоклимается то уже ничем не поможешь. Первый день дали таблетку не поднимаеться. Фермер:"Наверное придется зарезать". Свинья усливший это бежит к лошади говорит "вставай а то зарежут тебя". Лошадь не встает итак 4 дня. Пятый день лошадь встала хозяен радуется и говорит "По такому поводу зарегим свинью".']
+l = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFHJKLZXCBNMЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯСМИТЬБЮЁйцукенгшщзхъфывапролджэячсмитьбюё'
+bot = commands.Bot(command_prefix = '!', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
 @bot.command()
-async def add(ctx, left: int, right: int):
+async def slove_ex(ctx, example : str):
     """Adds two numbers together."""
-    await ctx.send(left + right)
+    await ctx.send(eval(example))
 
 @bot.command()
-async def repeat(ctx, times = 1, content='repeating...'):
+async def repeat(ctx, times = 1, content = 'repeating...'):
     """Repeats a message multiple times."""
     for i in range(times):
         await ctx.send(content)
@@ -26,6 +29,14 @@ async def repeat(ctx, times = 1, content='repeating...'):
 async def joined(ctx, member: discord.Member):
     """Says when a member joined."""
     await ctx.send(f'Hello {member.name} {discord.utils.format_dt(member.joined_at)}')
+
+@bot.command()
+async def rand_name(ctx, length = 10):
+    nick_name = ''
+    for i in range(length):
+        nick_name += random.choice(l)
+    
+    await ctx.send(nick_name)
 
 @bot.command()
 async def roll(ctx, dice: str):
@@ -39,11 +50,21 @@ async def roll(ctx, dice: str):
     result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
     await ctx.send(result)
 
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+
+@bot.command()
+async def anecdot(ctx):
+    await ctx.send(random.choice(anecdots))
+
 @bot.command()
 async def hello(ctx):
     await ctx.send(f'Привет! Я бот {bot.user}!')
 
+
 @bot.command()
 async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
-bot.run("MTE1MjYwMDQzMDEyNDIxMjI0NA.GQrjCi.gY5mr5ViW-65aYTHvU-p0ruYR5kDWj6k4t9ptA")
+bot.run("MTE1MjYwMDQzMDEyNDIxMjI0NA.G6HwLn.IhRZRvcczr2ZEkRUEhpHrICq_cUv-LcCDzqoAU")
