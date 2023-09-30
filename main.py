9170126a6290109db9e63e4820ac2ded07b1597c
Abduvohid <143887853+GodlikeNaruto@users.noreply.github.com>
2023-09-29 20:20:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -67,4 +67,4 @@ async def hello(ctx):
 @bot.command()
 async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
-bot.run("MTE1MjYwMDQzMDEyNDIxMjI0NA.G6HwLn.IhRZRvcczr2ZEkRUEhpHrICq_cUv-LcCDzqoAU")
+bot.run("Token")
